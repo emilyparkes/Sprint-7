2f1ec3829d8712c5b7a5aed69d975142f6911767
Emily Parkes <emilycoco@me.com>
2017-12-14 16:18:52 +1300

    complete

--- eloquent.js
+++ eloquent.js
@@ -32,8 +32,37 @@ var isEven = function(N) {
 
 // Bean Counting
 
+/* You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length with "s".length. The returned value will be a string containing only one character (for example, "b"). The first character has position zero, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+A loop in your function will have to look at every character in the string by running an index from zero to one below its length (< string.length). If the character at the current position is the same as the one the function is looking for, it adds 1 to a counter variable. Once the loop has finished, the counter can be returned.
+Take care to make all the variables used in the function local to the function by using the var keyword.*/
+
 var countBs = function(str){
-  (i = 0; i < length; i++)
+  var count = 0;
+  for (var i = 0; i < str.length; i++){
+    if (str.charAt(i) === "B"){
+      count++;
+    }
+  }
+  return count;
+}
+// → 2
+
+var countChar = function(str, char){
+  var count = 0;
+  for (var i = 0; i<str.length; i++){
+    if (str.charAt(i) === char){
+      count++;
+    }
+  }
+  return count;
 }
 
-var countChar = function()
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+console.log(countChar("BBC", "B"))
+// → 2
